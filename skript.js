ff6fab301b2a156a85e0cf4bca829693cbee4b2a
En programmerare <38755423+en-programmerare@users.noreply.github.com>
2021-08-10 23:44:09 +0200

    Uppdaterade vissa gymnasier till 21/22
    De andra får vi se om de kommer

--- skript.js
+++ skript.js
@@ -1,45 +1,55 @@
+const giltligaLov = ["host", "jul", "sport", "pask", "sommar"];
 let nu = new Date().getTime();
-let nedrakningsdatum;
-let cykler = 0;
+let nedrakningsdatum; //ställs in beroende senare
+let cykler = 0; //för animation, just nu oanvänt
+//argument från url
 let argument = window.location.href.indexOf("?") === -1 ? "" : window.location.href.substring(window.location.href.indexOf("?lov=") + 5);
+let autolov = argument.length === 0;
+let typ = ""; //för att göra "Nu är det HÖST/SPORT/JUL/SOMMAR/PÅSKlov"
+if(!giltligaLov.includes(argument)) //om argumentet är ogiltligt ska det ignoreras
+   autolov = true;
 
-beraknaNedrakningsdatum();
+beraknaNedrakningsdatum(); //ställ in grejer, se nedan
 rakna();
-setInterval(rakna, 1000);
+setInterval(rakna, 1000); //räkna ned varje sekund
 
+/*   STÄLL IN GREJER   */
+//Räkna ut vilket lov det räknas ned till, utifrån inställningar eller bara nästa lov
+//Ställer också in utseende, nedräkningsdatum mm
 function beraknaNedrakningsdatum() {
-   if(hostlovSlut - nu > 0 || argument === "host") {
+   if(autolov && hostlovSlut - nu > 0 || argument === "host") {
       nedrakningsdatum = hostlov;
+      typ = "höst";
       document.title = "Höstlovsnedräknaren";
       document.getElementById("nedrakning").classList.add("host");
       document.getElementsByTagName("body")[0].classList.add("hostBakgrund");
    }
-   else if(jullovSlut - nu > 0 || argument === "jul") {
+   else if(autolov && jullovSlut - nu > 0 || argument === "jul") {
       nedrakningsdatum = jullov;
       document.title = "Jullovsnedräknaren";
       document.getElementById("nedrakning").classList.add("jul");
       document.getElementsByTagName("body")[0].classList.add("julBakgrund");
    }
-   else if(sportlovSlut - nu > 0 || argument === "sport") {
+   else if(autolov && sportlovSlut - nu > 0 || argument === "sport") {
       nedrakningsdatum = sportlov;
       document.title = "Sportlovsnedräknaren";
       document.getElementById("nedrakning").classList.add("sport");
       document.getElementsByTagName("body")[0].classList.add("sportBakgrund");
    }
-   else if(pasklovSlut - nu > 0 || argument === "pask") {
+   else if(autolov && pasklovSlut - nu > 0 || argument === "pask") {
       nedrakningsdatum = pasklov;
       document.title = "Påsklovsnedräknaren";
       document.getElementById("nedrakning").classList.add("pask");
       document.getElementsByTagName("body")[0].classList.add("paskBakgrund");
    }
-   else if(sommarlovSlut - nu > 0 || argument === "sommar") {
+   else if(autolov && sommarlovSlut - nu > 0 || argument === "sommar") {
       nedrakningsdatum = sommarlov;
       document.title = "Sommarlovsnedräknaren";
       document.getElementById("nedrakning").classList.add("sommar");
       document.getElementsByTagName("body")[0].classList.add("sommarBakgrund");
    }
    else {
-      document.write("Inget lov inläst.");
+      document.getElementById("nedrakning").innerHTML = "Inget lov inläst.";
    }
 }
 function rakna() {
@@ -50,6 +60,7 @@ function rakna() {
    var minuter = Math.floor((avstand % (1000 * 60 * 60)) / (1000 * 60));
    var sekunder = Math.floor((avstand % (1000 * 60)) / 1000);
    
+   //lägg in animation här...
    if(avstand < 0) {
       cykler++;
       document.getElementById("nedrakning").innerHTML = "Nu är det lov!";
